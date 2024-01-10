527c30be71781f324d6fb103e7a20e4c5604ae65
AnthonyScott <anthony.scott1005@gmail.com>
2024-01-09 23:14:40 -0500

    Update the decoding function

--- pyramid_decoding.py
+++ pyramid_decoding.py
@@ -9,26 +9,25 @@ def decode(message_file):
         word = ' '.join(parts[1:])
         number_word[number] = word
 
-    max_number = max(number_word.keys())
 
     pyramid = []
     current = 1
     level = []
-    for i in range(1, max_number + 1):
+    for i in range(1, max(number_word.keys()) + 1):
         level.append(number_word[i])
         if i == current:
             pyramid.append(level)
             level = []
             current += len(pyramid) + 1
+ 
 
     decoded_message = pyramid[0][0]
-
     for i in range(1, len(pyramid)):
         decoded_message += ' ' + pyramid[i][-1]
 
     return decoded_message
 
 # Example usage:
-file_path = 'pyramid.txt' 
+file_path = 'coding_qual_input.txt' 
 decoded_message = decode(file_path)
 print(decoded_message)
